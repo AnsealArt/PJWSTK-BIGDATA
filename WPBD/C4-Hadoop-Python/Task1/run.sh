4ba686cdc33292b9da10b621685649cc2849afda
Filip Dzięcioł <48879563+AnsealArt@users.noreply.github.com>
2022-11-27 14:20:43 +0100

    Correct C4 Task1 scripts paths

--- WPBD/C4-Hadoop-Python/Task1/run.sh
+++ WPBD/C4-Hadoop-Python/Task1/run.sh
@@ -1,8 +1,8 @@
 /usr/local/hadoop/bin/hdfs dfs -mkdir input/lab4 || \
 /usr/local/hadoop/bin/hdfs dfs -put /home/filip/scripts/lab4/short_story.txt || \
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.4.jar \
--files /home/filip/scripts/lab4/mapper.py -mapper "python3 mapper.py" \
--files /home/filip/scripts/lab4/reducer.py -reducer "python3 reducer.py" \
+-file mapper.py -mapper "python3 mapper.py" \
+-file reducer.py -reducer "python3 reducer.py" \
 -input short_story.txt \
 -output count_words || \
 /usr/local/hadoop/bin/hdfs dfs -cat count_words_output.txt/* | sort -g -k 2,2 --reverse | head -n 10
